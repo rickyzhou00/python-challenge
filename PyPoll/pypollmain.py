326ddcd422caf399bc08e84b4ef5a308c5d39044
rickyzhou00 <zhou.ricky@hotmail.com>
2023-03-13 23:40:30 -0400

    commit

--- PyPoll/pypollmain.py
+++ PyPoll/pypollmain.py
@@ -8,24 +8,47 @@ filepath = os.path.join("python-challenge/PyPoll/Resources/election_data.csv")
 #setting variables
 totalvotes = 0
 candidatevotes = {}
-
-
+candidatelist = []
+winnervote = 0
+winner = ""
 #opening csv file
 with open(filepath) as csvfile:
-    csvreader = csv.DictReader(csvfile)
-
-    next(csvreader)
+    csvreader = csv.DictReader(csvfile,delimiter=",")
 
     for row in csvreader:
         totalvotes = totalvotes + 1
-        candidate = row[2]
 
-        if candidate in candidatevotes:
-            candidatevotes = candidatevotes[candidate] + 1
-        
-        else candidatevotes[candidate] = 1
-
+        candidates = row["Candidate"]
+        if candidates in candidatelist:
+            candidatevotes[candidates] = candidatevotes[candidates] + 1
+        else:
+            candidatelist.append(candidates)
+            candidatevotes[candidates] = 1
+    
 
 #output
-print(totalvotes)
-print(candidatevotes)
+print("Election Results")
+print("------------------------")
+print(f"Total Votes: {totalvotes}")
+
+
+
+#output to text
+outputpath = "pypolloutput.txt"
+with open(outputpath,'w') as file:
+    file.write("Election Results")
+    file.write("-------------------------")
+    file.write(f"Total Votes: {totalvotes}")
+    file.write("--------------------------")
+    
+
+    for candidates in candidatevotes:
+        voters = candidatevotes[candidates]
+        percentage = voters/totalvotes * 100
+        
+        if voters>winnervote:
+            winnervotes = voters
+            winner = candidates
+        output = (f"{candidates}:{percentage:3f}%({voters})")
+        print(output)
+        file.write(output)
